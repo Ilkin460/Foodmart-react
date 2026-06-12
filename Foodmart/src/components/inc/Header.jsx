66a4124ae75a124b75532448f7d888b39ffc5fb4
Ilkin460 <baxsiyev730@gmail.com>
2026-06-12 11:15:13 +0400

    Navbar

--- Foodmart/src/components/inc/Header.jsx
+++ Foodmart/src/components/inc/Header.jsx
@@ -6,12 +6,12 @@ import { IoMdArrowDropdown } from 'react-icons/io';
 
 const Header = () => {
   return (
-    <header className="w-full bg-white font-sans">
+    <header className="w-full bg-white font-sans sticky top-0 z-50 shadow-sm">
       
       <div className="max-w-360 mx-auto px-6 md:px-12 py-5 flex items-center justify-between gap-4">
         
         <div className="shrink-0">
-          <img src={logo} alt="Foodmart Logo" className="h-11 w-auto object-contain" />
+          <img src={logo} alt="Foodmart Logo" className="h-5 md:h-11 w-auto object-contain" />
         </div>
 
         <div className="hidden min-[993px]:flex grow max-w-162.5 items-center bg-[#F5F5F5] rounded-full px-5 py-2.5 mx-8">
@@ -27,13 +27,13 @@ const Header = () => {
             className="bg-transparent text-sm w-full outline-none text-gray-700 placeholder-gray-400"
           />
           <button className="text-gray-500 hover:text-gray-700 ml-2">
-            <FiSearch className="w-5 h-5" />
+            <FiSearch className="w-3 h-3 md:w-5 h-5" />
           </button>
         </div>
 
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2 md:gap-6">
           <button className="p-2.5 bg-[#F5F5F5] text-gray-700 rounded-full hover:bg-gray-200 min-[993px]:hidden">
-            <FiSearch className="w-5 h-5" />
+            <FiSearch className="w-3 h-3 md:w-5 h-5" />
           </button>
 
           <div className="text-right hidden lg:block mr-2">
@@ -42,11 +42,11 @@ const Header = () => {
           </div>
 
           <button className="p-2.5 bg-[#F5F5F5] text-gray-700 rounded-full hover:bg-gray-200 transition-colors">
-            <IoPersonOutline className="w-5 h-5" />
+            <IoPersonOutline className="w-3 h-3 md:w-5 h-5" />
           </button>
 
           <button className="p-2.5 bg-[#F5F5F5] text-gray-700 rounded-full hover:bg-gray-200 transition-colors">
-            <FiHeart className="w-5 h-5" />
+            <FiHeart className="w-3 h-3 md:w-5 h-5" />
           </button>
 
           <div className="text-left pl-2 cursor-pointer group select-none">
